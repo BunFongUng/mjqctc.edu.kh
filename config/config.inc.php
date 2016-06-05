38306a5b6b6bca89d8c17e030fb4f22990e4b83b
Bun Fong <bunfong@Buns-Mac-mini.local>
2016-06-05 10:29:12 +0700

    Update code

--- config/config.inc.php
+++ config/config.inc.php
@@ -16,5 +16,37 @@ if(stristr($_SERVER["HTTP_HOST"], "local") || (substr($_SERVER["HTTP_HOST"], 0, 
     $local = false;
 }
 
+//determine location of files and the URL of the sites:
+//Allow for development an different server
 
+if($local) {
+    //Always Debug when running local:
+    $debug = true;
 
+    define("IMG_PATH", "imgs");
+    define("BASE_URL", "http://localhost/mjqctc.edu.kh/");
+    define("DB_PATH", "config/Database.php");
+
+} else {
+    define("IMG_UPLOADS", "mjqctc_dashboard/uploads");
+    define("BASE_URL", "http://mjqctc.edu.kh/");
+    define("DB_PATH", "config/Database.php");
+}
+
+#Most important setting..
+#The $debug variable is used to set error management.
+#to debug a specific page, add this to the index.php page
+if($p == "thismodule") {
+    $debug = true;
+    require_once("config.inc.php");
+}
+
+//to debug the entire site, do
+$debug = true;
+
+//Assume debugging is off.
+if(!isset($debug)) {
+    $debug = false;
+}
+
+//end of the setting
